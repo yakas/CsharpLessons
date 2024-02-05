3831b9c6f571fe1987fa844092322dfaaf9a3fba
yakup atalay <yakupbraveheart@gmail.com>
2024-02-05 21:45:44 +0300

    codes are updated

---
+++ StaticMethods/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethods
+{
+    public class Customer
+    {
+        public string name;
+        public string surname;
+        public int age;
+
+        public void CustomerInfo()
+        {
+            Console.WriteLine("Customer name:"+name);
+            Console.WriteLine("Customer surname:" + surname);
+            Console.WriteLine("Customer age:"+age);
+        }
+
+        
+
+    }
+    
+}
---
+++ StaticMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethods
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Customer customer1= new Customer();
+            customer1.age = 55;
+            customer1.name = "yakup";
+            customer1.surname = "atalay";
+            customer1.CustomerInfo();
+
+        }
+    }
+}
