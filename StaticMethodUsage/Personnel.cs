02f414b5c049b3ed23076b32bdc72a8d3a670c1a
yakup atalay <yakupbraveheart@gmail.com>
2024-02-05 21:45:18 +0300

    codes are updated

---
+++ StaticMethodUsage/Personnel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethodUsage
+{
+    internal class Personnel
+    {
+        /*public void NameSurnameWrite()   static olmayan metodlarda ancak yeni bir nesne olusturup erisim yapabiliriz.
+        {
+            Console.WriteLine("Yakup Atalay");
+        }*/
+
+        public static void NameSurnameWrite() // ayni metodu sadece static yaptık.Bu sekilde Main metod'dan
+                                              // Personnel class'daki NameSurnameWrite() metoduna direk
+                                              // Personnel.NameSurnameWrite() yazarak erisebiliriz.
+                                              // static ifadesi eklendiginde degiskenler ve metodlar direk class üzerinden
+                                              //erisilebilir duruma gelir.
+        {
+            Console.WriteLine("Yakup Atalay");
+        }
+    }
+}
---
+++ StaticMethodUsage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethodUsage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*public void method usage
+            Personnel p1=new Personnel();
+            p1.NameSurnameWrite();
+            Console.ReadLine();
+
+
+             Personnel. yazarak NameSurnameWrite() metoda erisemiyoruz.Ancak class'dan yeni bir nesne olusturarak erisiyoruz
+             Cunku Personnel class'daki NameSurnameWrite() metod static degil
+            #####################################################################################
+            */
+            //public static void method usage
+             Personnel.NameSurnameWrite();
+             Console.ReadLine();
+
+        }
+    }
+}
