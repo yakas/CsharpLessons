2a1121aeded68aab05cc4f3f6fb6490322ea077d
yakup atalay <yakupbraveheart@gmail.com>
2024-02-05 22:51:17 +0300

    Static variable usage is added

---
+++ StaticVariableUsage/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVariableUsage
+{
+    public class Employee
+    {
+        public int employeeId;
+        public string employeeName;
+        public string employeeSurname;
+        public static int salary;
+
+
+    }
+}
---
+++ StaticVariableUsage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVariableUsage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.employeeId = 1;            //static olarak tanımlanmadıgı icin referans nesne olusturup kullanabiliriz
+            employee1.employeeName = "yakup";    //
+            employee1.employeeSurname = "atalay";//
+            Employee.salary = 5000;              // static olarak tanımlandığından refernas nesne olusturmadan kullandık.
+
+            Console.WriteLine("Employee id:"+employee1.employeeId);
+            Console.WriteLine("Employee name:"+employee1.employeeName);
+            Console.WriteLine("Employee surname:"+employee1.employeeSurname);
+            Console.WriteLine("Employee salary:"+Employee.salary);
+            Console.ReadLine();
+
+        }
+    }
+}
