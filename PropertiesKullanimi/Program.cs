e179b0f9f2da2efe63ad33ea90904c58f1cff1df
yakup atalay <yakupbraveheart@gmail.com>
2024-02-07 12:51:32 +0300

    get ve set property hakkinda aciklama ekledim

--- PropertiesKullanimi/Program.cs
+++ PropertiesKullanimi/Program.cs
@@ -9,7 +9,12 @@ namespace PropertiesKullanimiGetAndSetMethods
     internal class Program
     {
         static void Main(string[] args)
+
         {
+            // Property dedigimiz kavram olusturmus oldugumuz class'larda belirledigimiz fieldlara
+            // dısardan direk olarak deger atama ve goruntuleme yapılmasını ıstemedigimiz durumlarda kullanılır.
+            // Bir programcı olusturmus oldugu class icinde tanımlamıs oldugu field'lara dısardan müdehale edilmesini istemez.
+            // Bu fieldlara dolaylı yoldan ulaşılmasını ayarlayabilir.Bunun icin property get ve set metodları kullanılır.
             Musteri musteri1=new Musteri();
             musteri1.kilo1 = 100;// eger buradaki gibi deger atamasi yaparsak set bloklari calisir.
             int musterininKilosu = musteri1.kilo1; // burdaki gibi deger atamasi yapilmaz sadece degiskendeki deger cekilirse get bloklari calisir.
