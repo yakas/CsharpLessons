ca90b114b1055e4aafe42d3c540e0229c25c6797
yakup atalay <yakupbraveheart@gmail.com>
2024-02-07 22:09:47 +0300

    updated

--- InheritanceSample/Program.cs
+++ InheritanceSample/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using InheritanceSample;
 
 namespace InheritanceSample
 {
@@ -11,17 +13,17 @@ namespace InheritanceSample
         static void Main(string[] args)
         {
             
-                Cat cat1 = new Cat();
-                cat1.animalColor = "siyah";
-                cat1.animalName = "lazy";
-                cat1.animalType = "siyam";
-                cat1._animalAge = 3;
-                cat1.animalInfo();
-                Console.WriteLine(cat1._animalAge);
-                Console.ReadLine();
-
-                
-
+            Cat cat1 = new Cat();
+            cat1.animalColor = "siyah";
+            cat1.animalName = "lazy";
+            cat1.animalType = "siyam";
+            cat1._animalAge = 3;
+            cat1.animalInfo();
+            Console.WriteLine(cat1._animalAge);
+            Console.ReadLine();
+                                 
+                       
         }
+
     }
 }
