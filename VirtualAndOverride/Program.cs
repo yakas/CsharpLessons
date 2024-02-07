0caddcdfbfb6048cf05f0ef8d725ddb67df46250
yakup atalay <yakupbraveheart@gmail.com>
2024-02-08 00:02:34 +0300

    Virtual metod ve Override metod added

---
+++ VirtualAndOverride/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualAndOverride
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            b.OrnekMetod();
+            Console.Read();
+
+        }
+
+        class A
+        {
+            //public void OrnekMetod()
+            public virtual void OrnekMetod()  // virtual ibaresi eklenirse bu A sınıfı yani "base" alandaki 
+                                              // metodlar veya property'ler child class olan B de
+                                              // tekrardan düzenlenebilir.
+            {
+                Console.WriteLine("Bu bir örnek metotdur.");
+            }
+                          
+            
+        }
+
+        class B:A  // B classda A sınıftan gelen metod veya property'ler tekrar duzenlemek
+                   // icin child class olan B'de override ifadesi yazılıp bosluk tusuna basılırsa
+                   // base class'daki metod veya property'ler listelenir.Buradan istenilen metod veya
+                   // property secilir ve tab tusuna basılırsa otomatikmen override metod veya
+                   // property olusturulur.
+        {
+            public override void OrnekMetod() // buradaki override edilen metod aslında base class'da
+                                              // virtual olarak ayarlanmış metoddur.Buna ezilmiş metod
+                                              // denir.Yeniden düzenlenebilir.
+            {
+                Console.WriteLine("Bu bir ezilmiş override edilmis metod olmuştur."); 
+                
+            }
+
+        }
+    }
+}
---
+++ VirtualAndOverride/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("VirtualAndOverride")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VirtualAndOverride")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("674a16a3-7af8-41ab-959f-3332257f324b")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
