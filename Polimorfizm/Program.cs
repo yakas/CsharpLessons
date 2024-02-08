6a438377bf343aa99a6557dfd36a0b4091dc1bed
yakup atalay <yakupbraveheart@gmail.com>
2024-02-08 13:30:57 +0300

    polimorfizm updated

--- Polimorfizm/Program.cs
+++ Polimorfizm/Program.cs
@@ -6,9 +6,24 @@ using System.Threading.Tasks;
 
 namespace Polimorfizm
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
+        {
+            // A a = new A();  A class'dan yeni bir a nesnesi olusturduk. A dan A ya
+
+            A a = new B();  // A class tipinde B nesnesinden a objesini olusturduk  A dan B ye (Polimofizm).
+                            // Bu ilk olarak B türü örtülü olarak A türüne dönüştürülemez hatası verir.Bunu
+                            // önlemek için kalitim inheritence uygulamak gerekir.Bunun için B sınıfı A
+                            // sınıfından kalıtım alacak şekilde ayarlarız.
+            
+        }
+    class A
+        {
+        }
+    //class B
+
+    class B:A   // A a = new B() seklinde polimorfizm uygulamış olduk.
         {
         }
     }
