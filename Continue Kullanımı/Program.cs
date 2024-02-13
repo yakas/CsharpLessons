cb57f25568e5f084e4adcc28a3062f5682a76a8a
yakup atalay <yakupbraveheart@gmail.com>
2024-02-13 18:16:07 +0300

    Continue kullanımı eklendi

---
+++ Continue Kullanımı/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace Continue_Kullanımı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 != 0)
+                    continue; // continue deyimi ile alt satıra geçmez for dongusune doner.
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+
+            while (true)
+            {
+                if (true)
+                {
+                    switch (1)
+                    {
+                        case 5:
+                            continue; // burada continue case 5 doldugunda while dongusune tekrar doner
+                            break;
+                    }
+                }
+
+
+            }
+        }
+    }
+}
