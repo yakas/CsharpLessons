5e1d2c661bf7ae6e10e4511d2fece0210bb35d99
yakup atalay <yakupbraveheart@gmail.com>
2024-02-14 19:52:32 +0300

    switch keyword incelendi

--- SwitchCase/obj/Release/net8.0/SwitchCase.AssemblyInfo.cs
+++ SwitchCase/obj/Release/net8.0/SwitchCase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwitchCase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32643df4725665387904f01e7c4ea9d589a9dfa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db57151d716cafdfd17a0da7323087e35164467b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwitchCase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwitchCase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
