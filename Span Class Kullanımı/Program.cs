bb5477fa18cc40dec3950cfa3bf1b69af3d47922
yakup atalay <yakupbraveheart@gmail.com>
2024-02-15 13:57:22 +0300

    Span Class String text degerlerde ReadOnlySpan olarak erisim yapıldıgı gosterildi.

--- Span Class Kullanımı/Program.cs
+++ Span Class Kullanımı/Program.cs
@@ -20,7 +20,7 @@
             Span<int> span1 = sayilar;
             Span<int> span2 = new Span<int>(sayilar, 3, 5);// dizinin belirli bir alanını gösterdik.(dizi,start,uzunluk)
             
-            Span<int> span4 = sayilar.AsSpan();// diziden AsSpan() metodu ile span türünde bir nesne YANİ span4 'ü oluşturuyoruz..
+            Span<int> span4 = sayilar.AsSpan();// diziden AsSpan() metodu ile span türünde bir nesne YANİ span4 'ü döndurecektir..
             
             Span<int> span5 = sayilar.AsSpan(3, 5);// 40 50 60 70 80 çıktısını verir.
             span5[3] = 41; // dizinin fonksiyonları ile istenilen degere atama yapabiliyoruz.Dizinin tüm metodlarını kullanabilriz.
@@ -29,7 +29,14 @@
             {
                 Console.Write(item + " ");// 40 50 60 41 80 çıktısını verir.
             }
-            string text = "ben genellikle yaz aylarında balık tutmaktan hoşlanırım.";
+            //-------------------------------------------------------------------------------------------------------------------------------
+            string soz = "ben genellikle yaz aylarında balık tutmaktan hoşlanırım.";// metinsel ifadelerde span cinsinde değilde
+                                                                                    // ReadOnlySpan olarak erişebiliriz.
+            ReadOnlySpan<char> readonlyspan = soz.AsSpan(); // char olarak deger dondurur.
+            Console.Write(readonlyspan.Contains('a'));// oluşturulan readonlyspan nesnesi a char içeriyormu cevap true donecektir.
+            
+            
+            
 
         }
     }
